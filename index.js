49d794b1baea56869a173cb4a462077dfa12a1df
rovicxavier150 <rovicxavier150@gmail.com>
2025-05-15 04:22:02 +0000

    Simple test on Vercel deployment

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('gopd'); console.log('gopd required successfully');
+
 import express from 'express';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, push, set, update, get, orderByChild, query, equalTo, remove } from 'firebase/database';
